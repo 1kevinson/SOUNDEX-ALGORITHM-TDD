523089c997bc832ca390459be8cab475b5f21563
Kevin Kouomeu <59510116+1kevinson@users.noreply.github.com>
2022-09-22 01:00:25 +0200

    Incremental Steps

--- src/main/java/sample/Soundex.java
+++ src/main/java/sample/Soundex.java
@@ -1,8 +1,91 @@
 package sample;
 
+import java.util.HashMap;
+
 public class Soundex {
 
-    public String encode(String a) {
-        return "";
+    public static final int MAX_CODE_LENGTH = 4;
+
+    public String encode(String word) {
+        return zeroPad(head(word).concat(encodedDigits(tail(word))));
+    }
+
+    private String tail(String word) {
+        return word.substring(1);
+    }
+
+    private String head(String word) {
+        return word.substring(0, 1);
+    }
+
+    private String encodedDigits(String word) {
+        var encoding = "";
+
+        for (Character character: word.toCharArray()) {
+            if (isComplete(encoding)) break;
+
+            if (isTwoAdjacentLettersHaveTheSame(encoding, character)) {
+                encoding += encodedDigit(character);
+            }
+        }
+        return encoding;
+    }
+
+    private boolean isTwoAdjacentLettersHaveTheSame(String encoding, Character character) {
+        return !encodedDigit(character).equals(lastDigit(encoding));
+    }
+
+    private String lastDigit(String encoding) {
+        if (encoding.isEmpty()) return "";
+        return encoding.substring(encoding.length() - 1);
+    }
+
+    private Character upperFront(char letter) {
+        return Character.toUpperCase(letter);
+    }
+
+    private boolean isComplete(String encoding) {
+        return encoding.length() == MAX_CODE_LENGTH - 1;
+    }
+
+    public String encodedDigit(Character letter) {
+        var encodingsData = new HashMap<Character,String>();
+
+        // Encoding with 1
+        encodingsData.put('B',"1");
+        encodingsData.put('F',"1");
+        encodingsData.put('P',"1");
+        encodingsData.put('V',"1");
+
+        // Encoding with 2
+        encodingsData.put('C',"2");
+        encodingsData.put('G',"2");
+        encodingsData.put('J',"2");
+        encodingsData.put('K',"2");
+        encodingsData.put('Q',"2");
+        encodingsData.put('S',"2");
+        encodingsData.put('X',"2");
+        encodingsData.put('Z',"2");
+
+        // Encoding with 3
+        encodingsData.put('D',"3");
+        encodingsData.put('T',"3");
+
+        // Encoding with 4
+        encodingsData.put('L',"4");
+
+        // Encoding with 5
+        encodingsData.put('M',"5");
+        encodingsData.put('N',"5");
+
+        // Encoding with 6
+        encodingsData.put('R',"6");
+
+        return encodingsData.getOrDefault(upperFront(letter),"");
+    }
+
+    public String zeroPad(String word) {
+        final var numberOfZeroNeeded = MAX_CODE_LENGTH - word.length();
+        return word.concat("0".repeat(numberOfZeroNeeded));
     }
 }
--- src/test/java/sample/SoundexTest.java
+++ src/test/java/sample/SoundexTest.java
@@ -1,18 +1,74 @@
 package sample;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class SoundexTest {
 
+    Soundex soundex;
+
+    @BeforeEach
+    void setUp() {
+        soundex = new Soundex();
+    }
+
     @Test
+    @Order(1)
     void should_return_sole_letter_of_one_letter_word() {
-        Soundex soundex = new Soundex();
+        assertThat(soundex.encode("A")).isEqualTo("A000");
+    }
 
-        final var encoded = soundex.encode("A");
+    @Test
+    @Order(2)
+    void should_pads_with_zero_to_ensure_three_digits() {
+        assertThat(soundex.encode("I")).isEqualTo("I000");
+    }
 
+    @Test
+    @Order(3)
+    void should_replace_consonants_with_appropriate_digits() {
+        assertThat(soundex.encode("AX")).isEqualTo("A200");
+    }
+
+    @Test
+    @Order(4)
+    void should_ignore_non_alphabetic() {
+        assertThat(soundex.encode("A#")).isEqualTo("A000");
+    }
+
+    @Test
+    @Order(5)
+    void should_replace_multiple_consonants_with_digits() {
+        assertThat(soundex.encode("ACDL")).isEqualTo("A234");
+    }
+
+    @Test
+    @Order(6)
+    void should_limit_length_to_four_characters() {
+        assertThat(soundex.encode("DCDLB")).hasSize(4);
+    }
+
+    @Test
+    @Order(7)
+    void should_ignore_vowels() {
+        assertThat(soundex.encode("BAEIOUHYCDL")).isEqualTo("B234");
+    }
+
+    @Test
+    @Order(8)
+    void should_combine_duplicate_encodings() {
+        assertThat(soundex.encodedDigit('B')).isEqualTo(soundex.encodedDigit('F'));
+        assertThat(soundex.encodedDigit('C')).isEqualTo(soundex.encodedDigit('G'));
+        assertThat(soundex.encodedDigit('D')).isEqualTo(soundex.encodedDigit('T'));
+
+        assertThat(soundex.encode("ABFCGDT")).isEqualTo("A123");
+    }
+
+    @Test
+    @Order(9)
+    void should_ignore_double_letters_and_voyels() {
+        assertThat(soundex.encode("BAAeEiIoOuUHHyYcdl")).isEqualTo("B234");
     }
 }
